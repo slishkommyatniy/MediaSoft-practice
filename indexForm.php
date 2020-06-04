dfb37dd9486fbca7adbf34389a1309d11f9eb3e4
slishkommyatniy <nikonorov.maksim@bk.ru>
2020-06-04 13:12:07 +0400

    Fix </p>

--- indexForm.php
+++ indexForm.php
@@ -1,5 +1,5 @@
 <form action="Form.php" method = "post" enctype = "multipart/form-data">
     <input type = "file" name = "text" > <br>
     <textarea name = "description" ></textarea> <br>
-    <button type="submit">Отправить форму</button></p>
+    <button type="submit">Отправить форму</button>
 </form>
